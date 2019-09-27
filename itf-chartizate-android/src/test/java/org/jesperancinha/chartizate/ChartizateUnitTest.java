32c453701cfdc59efa291b9f9a4fd83a07c3ffa0
Joao Esperancinha <jofisaes@gmail.com>
2019-09-27 10:14:46 +0200

    Cleans up test environment

--- itf-chartizate-android/src/test/java/org/jesperancinha/chartizate/ChartizateUnitTest.java
+++ itf-chartizate-android/src/test/java/org/jesperancinha/chartizate/ChartizateUnitTest.java
@@ -1,8 +1,8 @@
 package org.jesperancinha.chartizate;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
