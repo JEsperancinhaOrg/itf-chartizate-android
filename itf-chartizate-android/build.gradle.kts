f6603b42fb0fd6dfb599ff5aec6e58f39b953d0e
jesperancinha <jofisaes@gmail.com>
2025-09-11 10:08:47 +0200

    Fixed the build

--- itf-chartizate-android/build.gradle.kts
+++ itf-chartizate-android/build.gradle.kts
@@ -52,5 +52,13 @@ tasks.register<JacocoReport>("jacocoTestReport") {
 
 dependencies {
     implementation(libs.chartizate)
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+    failOnNoDiscoveredTests = false
+}
